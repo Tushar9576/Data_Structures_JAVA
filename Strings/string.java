32daa4dd933590adfa967fe8f16aa410e9f0f568
Tushar Kant <e2k19103780@pictsctr.onmicrosoft.com>
2022-06-21 08:44:48 +0530

    Add files via upload

---
+++ Strings/string.java
@@ -0,0 +1,13 @@
+public class string {
+    public static void main(String[] args) {
+        String a = "tushar";
+        String b = "tushar";
+
+        System.out.println(a == b);
+
+        String name1 = new String("Tushar");
+        String name2 = new String("Tushar");
+        System.out.println(name1.equals(name2));
+
+    }
+}
