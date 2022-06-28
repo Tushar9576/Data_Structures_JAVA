34966b867c4d4e59a7ab57508e287ff5d672e11e
Tushar Kant <e2k19103780@pictsctr.onmicrosoft.com>
2022-06-29 02:13:35 +0530

    Add files via upload

---
+++ Strings-I/palindrom.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+
+public class palindrom {
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(ispalindrom(str));
+
+    }
+    static boolean ispalindrom(String str){
+        if (str == null || str.length() == 0){
+            return true;
+        }
+
+        str = str.toLowerCase();
+        for (int i = 0; i <= str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+            if (start != end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
