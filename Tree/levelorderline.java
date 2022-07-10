8cf91454381698ccf42d9228f6c55cb8019b9c0f
Tushar Kant <e2k19103780@pictsctr.onmicrosoft.com>
2022-07-10 17:28:34 +0530

    Add files via upload

---
+++ Tree/levelorderline.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class levelorderline {
+    static class Node{
+        int key;
+        Node left;
+        Node right;
+        public Node(int k) {
+            key = k;
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+        root.left.left = new Node(8);
+        root.left.right = new Node(7);
+        root.left.right.left = new Node(9);
+        root.left.right.right = new Node(15);
+        root.right.right  =new Node(6);
+        printlevelline(root);
+    }
+    static void printlevelline(Node root){
+        if(root == null){
+            return ;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        q.add(null);
+        while (q.size() > 1){
+            Node curr = q.poll();
+            if(curr == null){
+                System.out.println();
+                q.add(null);
+                continue;
+            }
+            System.out.println(root.key + " ");
+            if(curr.left != null){
+                q.add(curr.left);
+            }
+            if(curr.right != null){
+                q.add(curr.right);
+            }
+        }
+    }
+
+}
